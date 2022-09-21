7215d4ac4f447f98c9d75726689bd27af7feae54
11Larsson11 <1richardlarsson1@gmail.com>
2022-09-21 12:08:54 +0200

    Updated form

--- src/components/NewReview/NewReview.js
+++ src/components/NewReview/NewReview.js
@@ -1,18 +1,38 @@
+import React, { useState } from "react";
 import ReviewForm from "./ReviewForm";
 import "./NewReview.css";
 
 const NewReview = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveReviewDataHandler = (enteredReviewData) => {
     const reviewData = {
       ...enteredReviewData,
       id: Math.random().toString(),
     };
     props.onAddReview(reviewData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-review">
-      <ReviewForm onSaveReviewData={saveReviewDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new book review</button>
+      )}
+      {isEditing && (
+        <ReviewForm
+          onSaveReviewData={saveReviewDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,27 +1,28 @@
 import React, { useState } from "react";
-import ListItem from "./ListItem";
 import ReviewFilter from "./ReviewFilter";
 import Card from "../UI/Card";
 import "./Reviews.css";
+import ReviewList from "./ReviewList";
 
 const Reviews = (props) => {
-const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredReviews = props.items.filter((review) => {
+    return review.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="reviews">
-        <ReviewFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ListItem
-          date={props.items[0].date}
-          bookName={props.items[0].bookName}
-          author={props.items[0].author}
-          description={props.items[0].description}
-          review={props.items[0].review}
+        <ReviewFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
+        <ReviewList items={filteredReviews} />
       </Card>
     </div>
   );
